186df49357646189a545b341547b489d2820c88c
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-12-05 16:49:22 +0530

    Added Functionalities

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
 
 var express = require('express')
-
+var bodyParser = require('body-parser')
 var app = express()
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+var message = {
+
+}
+
+app.get('/message', (req, res) => {
+    res.send(message)
+})
+
+app.post('/message', (req, res) => {
+    message.push(req.body)
+    res.sendStatus(200)
+})
+
+
 var server = app.listen(3000, () => {
     console.log('Server is on', server.address().port)
 })
+
+
