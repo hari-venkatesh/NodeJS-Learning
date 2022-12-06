64f5e88b781f5e9824ef9ec9f1d6e89d94c05764
hari-venkatesh <58521420+hari-venkatesh@users.noreply.github.com>
2022-12-06 23:07:39 +0530

    Added Api calls

--- server.js
+++ server.js
@@ -7,12 +7,28 @@ app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-var message = {
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
-}
+mongoose.Promise = Promise
+
+var message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
 
 app.get('/message', (req, res) => {
-    res.send(message)
+    Message.find({}, (err, messages) => {
+        res.send(messages)
+    })
+})
+
+app.get('/messages/:user', (req, res) => {
+    var user = req.params.user
+    Message.find({name: user}, (err, messages) => {
+        res.send(messages)
+    })
 })
 
 app.post('/message', (req, res) => {
